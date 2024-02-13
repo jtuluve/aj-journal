4a76fd00a57b78536d6425b072d80a201a8dfe6b
Gauresh G Pai <paigauresh@gmail.com>
2024-02-13 15:16:07 +0530

    styling and a few buttons with their corresponding data div added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,3 @@
-import '../../styles/about.css'
 
 const About = () => {
   return (
@@ -9,7 +8,10 @@ const About = () => {
           <button>Journal</button>
           <button>Aim &amp; Scope</button>
           <button>Indexing</button>
+          <button>Peer reviewing process</button>
+          <button>Licencing &amp; Copyright Policy</button>
           <button>AJIMS Research Center</button>
+          <button>Disclaimer</button>
           {/* Content for different sections */}
           <div id="generalSection" className="about">
             <h2>About the Journal</h2>
@@ -22,18 +24,30 @@ const About = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sequi quae, obcaecati quasi atque consequuntur
               culpa dolore beatae. Repellat modi veniam magnam animi dolorem.</p>
           </div><br /><br />
-          <div id="submissionSection" className="about">
+          <div id="aimSection" className="about">
             <h2>Aim &amp; Scope</h2>
             <p>Aim &amp; Scope.</p>
           </div>
-          <div id="editorialSection" className="about">
-            <h2>Indexing</h2>
-            <p>Indexing.</p>
+        <div id="indexingSection" className="about">
+          <h2>Indexing</h2>
+          <p>Indexing.</p>
+        </div>
+          <div id="peerSection" className="about">
+            <h2>Peer reviewing process</h2>
+            <p>Peer reviewing process.</p>
           </div>
-          <div id="technicalSection" className="about">
+        <div id="licenceSection" className="about">
+          <h2>Licencing &amp; Copyright Policy</h2>
+          <p>Licencing &amp; Copyright Policy.</p>
+        </div>
+          <div id="AJIMSRCSection" className="about">
             <h2>AJIMS Research Center</h2>
             <p>About for AJIMS Research Center.</p>
           </div>
+        <div id="disclaimerSection" className="about">
+          <h2>Disclaimer</h2>
+          <p>Disclaimer.</p>
+        </div>
         </div>
       </div>
   )
